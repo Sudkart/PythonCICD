61a4fd5c870610e80a28413576893b82e9be5c67
Sudharshan Karthik <globusitsudharshan@gmail.com>
2023-08-03 21:20:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to Python CI CD!'
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__=="__main__":
+    app.run(debug=True)
