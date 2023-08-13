de87f567e7bbe6b7691418b4773c8695a0f5008b
Sudharshan Karthik <globusitsudharshan@gmail.com>
2023-08-13 23:51:09 +0530

    changes for getting the output from the model

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 
 app = Flask(__name__, template_folder="template")
+
+model = tf.keras.models.load_model('dnn_model.h5')
+tokenizer = Tokenizer()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        input_text = request.form['input_text']
+        print("input_text:"+input_text)
+        tokenizer.fit_on_texts([input_text])
+        sequences = tokenizer.texts_to_sequences([input_text])
+        padded_sequences = pad_sequences(sequences,padding='post', maxlen=tokenizer.num_words) 
+        # Make a prediction using the loaded model
+        prediction = model.predict(padded_sequences)
+
+        # Convert ndarray to a serializable list
+        prediction_list = prediction.tolist()
+        
+        return jsonify({'result': prediction_list})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
 
 
 if __name__=="__main__":
